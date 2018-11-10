e488fd349d9a6928a02c6679b2e06a4cc6981f10
Justin Nguyen <2014justinnguyen@gmail.com>
2018-11-10 13:29:14 -0600

    added the run_pintos_tests.sh script to filesys dir

---
+++ filesys/run_pintos_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TEST_FILES='
+args-none
+dir-open
+'
+
+for file in $TEST_FILES; do
+
+    # add line spacing between tests
+    echo ""
+
+    # find which folder the test is in
+    DIR=`find . -name $file`
+
+    if [ -z "$DIR" ]; then
+        echo "ERROR: test not found"
+    else
+        # remove the old output file, run the test, output results
+        rm $DIR.output 2> /dev/null
+        make $DIR.result 2> /dev/null
+        cat $DIR.output 2> /dev/null
+        cat $DIR.result 2> /dev/null
+    fi
+
+done
